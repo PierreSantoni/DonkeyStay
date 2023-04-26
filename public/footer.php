2855f7a13c10fa2f3cca5ea15b54e9897e858f35
Dorian RONDI <dorianrondi@gmail.com>
2023-04-26 15:50:03 +0200

    alpha footer.php

---
+++ public/footer.php
@@ -0,0 +1,15 @@
+    </main>
+    <footer class="footer mt-auto py-3 bg-donkey-primary sticky">
+        <div class="container-fluid">
+            <div class="text-center">
+                <img src="donkey.png" alt="DonkeyStay Logo" title="DonkeyStay Alive">
+                <h5 class="h1">DonkeyStay</h5>
+            </div>
+            <div class="hr mt-3 mb-3"></div>
+        </div>
+    </footer>
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</body>
+</html>
