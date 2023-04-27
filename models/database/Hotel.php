c5c4c33d7f29cf03ec0794366b49292a3d4a8358
Dorian RONDI <dorianrondi@gmail.com>
2023-04-27 16:59:35 +0200

    Accès a la base de données et Lecture de tables Opérationnel

--- models/database/Hotel.php
+++ models/database/Hotel.php
@@ -1,2 +1,17 @@
 <?php 
 namespace App\models\database;
+
+class Hotel extends DataBaseModel
+{
+    private $hotelID;
+    private $hotelStars;
+    private $hotelName;
+    private $hotelAdress;
+    private $hotelCity;
+    private $hotelCountry;
+
+    public function __construct()
+    {
+        $this->table = 'hotel';
+    }
+}
--- public/list.php
+++ public/list.php
@@ -1,2 +1,19 @@
+<?php
+
+use App\models\Autoloader;
+use App\models\database\Hotel;
+use App\models\database\User;
+use App\models\database\Reservation;
+use App\models\database\Room;
+
+require_once '../models/Autoloader.php';
+Autoloader::register();
+
+$hotel = new Hotel;
+?>
+
 <?php require 'header.php'; ?>
+<pre>
+<?php print_r($hotel->findAll()); ?>
+</pre>
 <?php require 'footer.php'; ?>
