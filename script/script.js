11033cf79bfeee1506af5026f39049eb161f5ae5
Oualid <medjidel.oualid@gmail.com>
2023-04-27 13:44:55 +0200

    block previous calendar dates

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+    
+    var arrivee = document.getElementById('arrivee');
+    var depart = document.getElementById('depart');
+     
+    arrivee.setAttribute('min', new Date().toISOString().split('T')[0]);
+    depart.setAttribute('min', new Date().toISOString().split('T')[0]);
+    
+    arrivee.addEventListener('change', function() {
+        depart.setAttribute('min', arrivee.value);
+    });
+
