a3fad3ac4a81a3acefb4c270d8d0104c3c01601e
Dorian RONDI <dorianrondi@gmail.com>
2023-04-27 14:02:05 +0200

    mise a jour header & footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 </main>
-<!-- Footer -->
+    <!-- Footer -->
     <footer class="text-center text-lg-start text-white bg-donkey-primary">
         <!-- Grid container -->
         <div class="container p-4 pb-0">
@@ -9,52 +9,26 @@
                 <div class="row">
                     <!-- Grid column -->
                     <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">
-                            Donkeystay
-                        </h6>
-                        <p>
-                            You have just selected to stay
-                            at one of the top
-                            25 hotels in the word
-                        </p>
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Donkeystay</h6>
+                        <p>You have just selected to stay at one of the top 25 hotels in the word</p>
                     </div>
                     <!-- Grid column -->
-
                     <hr class="w-100 clearfix d-md-none" />
-
                     <!-- Grid column -->
                     <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
                         <h6 class="text-uppercase mb-4 font-weight-bold">About</h6>
-                        <p>
-                            <a class="text-white" href="#">About Us</a>
-                        </p>
-                        <p>
-                            <a class="text-white" href="#">Careers</a>
-                        </p>
-                        <p>
-                            <a class="text-white" href="#">Contact Us</a>
-                        </p>
-                        
+                        <p><a class="text-white" href="#">About Us</a></p>
+                        <p><a class="text-white" href="#">Careers</a></p>
+                        <p><a class="text-white" href="#">Contact Us</a></p>
                     </div>
                     <!-- Grid column -->
-
                     <hr class="w-100 clearfix d-md-none" />
-
                     <!-- Grid column -->
                     <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">
-                            reservations
-                        </h6>
-                        <p>
-                            <a class="text-white" href="#">Request an invoice</a>
-                        </p>
-                        <p>
-                            <a class="text-white" href="#">Find a reservation</a>
-                        </p>
-                        <p>
-                            <a class="text-white" href="#">Email Preferences</a>
-                        </p>
-                        
+                        <h6 class="text-uppercase mb-4 font-weight-bold">reservations</h6>
+                        <p><a class="text-white" href="#">Request an invoice</a></p>
+                        <p><a class="text-white" href="#">Find a reservation</a></p>
+                        <p><a class="text-white" href="#">Email Preferences</a></p>
                     </div>
 
                     <!-- Grid column -->
@@ -73,9 +47,7 @@
                 <!--Grid row-->
             </section>
             <!-- Section: Links -->
-
             <hr class="my-3">
-
             <!-- Section: Copyright -->
             <section class="p-3 pt-0">
                 <div class="row d-flex align-items-center">
@@ -89,27 +61,16 @@
                         <!-- Copyright -->
                     </div>
                     <!-- Grid column -->
-
-                    <!-- Grid column -->
                     <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-                        <!-- Facebook -->
-                        
-                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button" href="https://www.facebook.com/"
-                            target="_blank"><i class="fab fa-facebook-f"></i></a>
 
+                        <!-- Facebook -->
+                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
 
                         <!-- Twitter -->
-                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button" href="https://twitter.com/"
-                            target="_blank"><i class="fab fa-twitter"></i></a>
-
-
-                        
+                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button" href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
 
                         <!-- Instagram -->
-                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button" href="https://www.instagram.com/"
-                            target="_blank"><i class="fab fa-instagram"></i></a>
-
-
+                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button" href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
                     </div>
                     <!-- Grid column -->
                 </div>
@@ -120,20 +81,9 @@
     </footer>
     <!-- Footer -->
 
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+    </body>
 
-
-
-
-
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
-        integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
-        integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+    </html>
