34bfa05bdf832de520e2eb70b774443f6f19f37e
Oualid <medjidel.oualid@gmail.com>
2023-05-02 09:30:47 +0200

    create careers.php page from footer

---
+++ aboutUs.php
@@ -0,0 +1,161 @@
+<?php include "header.php"
+?>
+
+<section class="about-us-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1 class="text-donkey-primary">About Us</h1>
+                <p class="lead">At Donkeystay, we believe that traveling should be a hassle-free and enjoyable experience for everyone. That's why we've created a leading hotel reservation platform that offers a seamless booking process and a wide range of accommodations to choose from.</p>
+                <p>We're passionate about making travel accessible and affordable for everyone, which is why we work hard to offer competitive rates and unbeatable deals. Our platform is designed to make it easy for anyone to find and book their ideal accommodation, no matter where they're headed in the world.</p>
+                <a href="#team" class="btn btn-donkey-primary mt-3">Meet Our Team</a>
+            </div>
+            <div class="col-md-6">
+                <img src="/img/aboutUs2.jpg" alt="About Us" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Features Section -->
+<section class="features-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h4 class="card-title">User-Friendly Platform</h4>
+                        <p class="card-text">Our platform is designed to be user-friendly and easy to use, making it simple for anyone to find and book their ideal accommodation.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h4 class="card-title">Wide Range of Accommodations</h4>
+                        <p class="card-text">We offer a wide range of accommodations to choose from, including hotels, apartments, and vacation rentals.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h4 class="card-title">Competitive Rates and Deals</h4>
+                        <p class="card-text">We work hard to offer competitive rates and unbeatable deals to our customers, making travel accessible and affordable for everyone.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Team Section -->
+<section class="team-section" id="team">
+    <div class="container">
+        <h2 class="text-donkey-primary mb-4">Meet Our Team</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <img src="/img/aboutUs1.jpg" alt="Team Member" class="img-fluid rounded-circle mb-3">
+                        <h4 class="card-title">Sarah CONNOR</h4>
+                        <p class="card-text">CEO</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <img src="img/aboutUs.jpg" alt="Team Member" class="img-fluid rounded-circle mb-3">
+                        <h4 class="card-title">Marlene SASSEUR</h4>
+
+                        <p class="card-text">Marlene is responsible for the day-to-day operations and management of Donkeystay. With over 15 years of experience in the hospitality industry, she brings a wealth of knowledge and expertise to the team.</p>
+                        <ul class="list-unstyled">
+                            <li><i class="fas fa-envelope mr-2"></i>marlene@donkeystay.com</li>
+                            <li><i class="fas fa-phone mr-2"></i>(555) 555-1212</li>
+                        </ul>
+                        <div class="social-links">
+                            <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+                            <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+
+                            <a href="#" class="linkedin"><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- Testimonials Section -->
+<section class="testimonials-section">
+    <div class="container">
+        <h2 class="text-donkey-primary mb-4">What Our Customers Are Saying</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <p class="card-text">"I've been using Donkeystay for all of my business travel needs for the past year, and I couldn't be happier. The platform is easy to use, and the customer service is top-notch."</p>
+                        <div class="testimonial-author">
+                            <img src="img/client.jpg" alt="Customer" class="img-fluid rounded-circle mr-3">
+                            <div class="author-info">
+                                <h5 class="mb-0">Trinity WALKER</h5>
+                                <p class="text-muted">CEO, ABC Company</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <p class="card-text">"As someone who travels frequently for leisure, I appreciate the wide selection of hotels available on Donkeystay. The platform makes it easy to find great deals on hotels in the locations I want to visit."</p>
+                        <div class="testimonial-author">
+                            <img src="img/client1.jpg" alt="Customer" class="img-fluid rounded-circle mr-3">
+                            <div class="author-info">
+                                <h5 class="mb-0">Jane Doe</h5>
+                                <p class="text-muted">Freelance Writer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <p class="card-text">"Donkeystay has revolutionized the way I book hotels for my family vacations. I can easily compare prices and amenities, and I know I'm getting the best deal possible."</p>
+                        <div class="testimonial-author">
+                            <img src="img/client2.jpg" alt="Customer" class="img-fluid rounded-circle mr-3">
+                            <div class="author-info">
+                                <h5 class="mb-0">Peggy LACOCHONE</h5>
+                                <p class="mb-0 text-muted">Co-Founder & CEO</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- Call to Action Section -->
+<section class="call-to-action-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-8">
+                <h2 class="text-white mb-4">Join Our Team</h2>
+                <p class="lead text-white">We are always looking for talented individuals to join our team and help us grow. If you are passionate about travel and technology, we want to hear from you.</p>
+            </div>
+            <div class="col-md-4 text-md-right">
+                <a href="#jobs" class="btn btn-lg btn-outline-light mt-3">View Open Positions</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<?php include "footer.php"
+?>
--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
                 <!-- Grid column -->
                 <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
                     <h6 class="text-uppercase mb-4 font-weight-bold">About</h6>
-                    <p><a class="text-white" href="#">About Us</a></p>
+                    <p><a class="text-white" href="/aboutUs.php">About Us</a></p>
                     <p><a class="text-white" href="careers.php">Careers</a></p>
                     <p><a class="text-white" href="contact.php">Contact Us</a></p>
                 </div>
