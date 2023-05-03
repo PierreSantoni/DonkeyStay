800d4c51ebe23715e614ffff3bc975fdb1cfa30e
DorianRONDI <dorianrondi@gmail.com>
2023-05-03 12:48:31 +0200

    Architecture MVC terminée

--- views/hotel/index.php
+++ views/hotel/index.php
@@ -2,27 +2,29 @@
 use App\models\Room;
 
 $rooms = new Room;
-
 ?>
 <div class="container-fluid">
     <h4>Utilisateurs enregistrés</h4>
     <div class="row">
-        <?php foreach ($hotels as $key => $value) : ?>
-            <?php
-            //$room = $rooms->findByID();
-            ?>
+        <?php foreach ($hotels as $hotelValue) : ?>
+            <?php $room = $rooms->findBy(['hotelID' => $hotelValue->hotelID]); ?>
 
         <article>
             <h5>
-                <a href="hotel/hotelProfil/<?= $value->hotelID; ?>">
-                    <?= $value->hotelName; ?>
+                <a href="hotel/hotelProfil/<?= $hotelValue->hotelID; ?>">
+                    <?= $hotelValue->hotelName; ?>
                 </a>
             </h5>
-            <p><?= $value->hotelStars; ?> Etoiles</p>
-            <p><?= $value->hotelAdress . " ," . $value->hotelCity . " ," . $value->hotelCountry; ?></p>
+            <p>hotel <?= $hotelValue->hotelStars; ?> Etoiles</p>
+            <p>Situé <?= $hotelValue->hotelAdress . " ," . $hotelValue->hotelCity . " ," . $hotelValue->hotelCountry; ?></p>
+            <p><strong>Cette hotel propose les chambes suivante :</strong></p>
+            <ul>
+                <?php foreach ($room as $roomValue) : ?>
+                    <li>La chambre n°<?= $roomValue->roomNumber; ?> à <?= $roomValue->roomNightPrice; ?> € la nuit</li>
+                <?php endforeach; ?>
+            </ul>
         </article>
 
         <?php endforeach; ?>
-        
     </div>
 </div>
